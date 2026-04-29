022f9fd3b39438e6b99bcf684919c21908639c11
Soken-hanaya <soken.hanaya.m@gmail.com>
2026-04-29 20:53:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,11 @@ const urlsToCache = [
   '公共測量の手続.html',
   '作業規程の準則 総則.html',
   '作業規程の準則 基準点測量.html',
+  '作業規程の準則 GNSS標高測量.html',
+  '作業規程の準則 路線測量.html',
+  '作業規程の準則 河川測量.html',
+  '作業規程の準則 用地測量.html',
+  'JPGIS.html',
   'manifest.json',
   'sw.js'
 ];
